67493885243e290b322c63b3bf8afa6451d4b994
Paul Eppers <pauleppers@hotmail.com>
2020-12-20 13:37:20 -0500

    added gauge plot

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,33 +1,37 @@
-//submit repository and web links
+// create variables that allow both global and local access
 var names = [];
 var metadata = [];
 var samples = []
 
-d3.json("samples.json").then((data) => {
-    console.log(data);
-    // add the dropdown options for all the participants
+// pull in the JSON data file, load data into variables, 
+d3.json("../../../samples.json").then((data) => {
+    // load data variable, used globally
     names = data.names;
     metadata = data.metadata;
     samples = data.samples;
     var selOption = d3.select("#selDataset");  // capture input for change and value changed to
 
+    // add the ids (names data) to the drop down to allow user to change person in test
     Object.entries(names).forEach(([index, name])=> {
         var cell = selOption.append("option");    // create option element
-        cell.text(name);  
-    });  // update text of the option element
-    setDemographic(940);
-    barPlot(940);
-    bubbleChart(940);
-    
+        cell.text(name);                        // update text of the option element
+    });  
+    optionChanged(940);
 });
 
+// functions to create the 4 data visualization on index.html, reference   https://plot.ly/javascript/
+function optionChanged(selected) {
+    setDemographic(selected);
+    barPlot(selected);
+    bubbleChart(selected);
+    guageChart(selected);
+};  
+
+// create the drop down and populate the demographics list on index.html
 function setDemographic(selectedId){
-    console.log("selectedID:", metadata);   
     var personalData = metadata.filter(function(person){  // parse the data
         return person.id == selectedId;
     });
-    //console.log("personal data:", personalData);
-    console.log("selected22222:", personalData);
     var id = personalData[0].id;
     var ethnicity = personalData[0].ethnicity;
     var gender = personalData[0].gender;
@@ -36,15 +40,14 @@ function setDemographic(selectedId){
     var bbtype = personalData[0].bbtype;
     var wfreq = personalData[0].wfreq;
 
-    // Then, select the unordered list element by class name
+    // Select the unordered list element by class name
     var list = d3.select("#sample-metadata");
 
-    // remove any children from the list to
+    // remove any children from the list
     list.html("");
 
-    // append stats to the list
+    // append stats to the list 'Demographic Info' of index.html
     list.append("h5").text(`id: ${id}`);
-    console.log("id:", id)
     list.append("h5").text(`ethnicity: ${ethnicity}`);
     list.append("h5").text(`gender: ${gender}`);
     list.append("h5").text(`age: ${age}`);
@@ -53,68 +56,53 @@ function setDemographic(selectedId){
     list.append("h5").text(`wfreq: ${wfreq}`);
 }
 
-// reference   https://plot.ly/javascript/
-function optionChanged(selected) {
-    console.log("selected55:", selected);
-    setDemographic(selected);
-    barPlot(selected);
-    bubbleChart(selected);
-};  
-
-// updates the html page index.html with the raw data when page is loaded
-// and also updates html page with filtered data
-
-// // create horizontal bar chart
+// // create horizontal bar chart and place it in index.html
 function barPlot(selectedId){
     var sampleData = samples.filter(function(sample){  // parse the data
         return sample.id == selectedId;
     });
     
-    otu = sampleData[0].otu_ids.slice(0,10);
-    otu = otu.map(id => `OTU  ${id}`);
-    otu = otu.reverse();
-    samp_values = sampleData[0].sample_values.slice(0,10);
+    otu = sampleData[0].otu_ids.slice(0,10);    // getting the top 10 bacteria
+    otu = otu.map(id => `OTU  ${id}`);          // creating text string
+    otu = otu.reverse();                        // reverse order, largest value on top
+    samp_values = sampleData[0].sample_values.slice(0,10);  // same steps as otu variable
     samp_values = samp_values.reverse();
-    console.log(otu);
-    console.log(samp_values);
 
     var trace = {
         x: samp_values,
         y: otu,
-        type: 'bar',   // line, scatter = mode:”markers”
+        type: 'bar',  
         orientation: 'h',
-          }
+          };
+
     var layout = {
         title: 'Top 10 OTUs',
-        }
+        };
+
     Plotly.newPlot("bar", [trace], layout);
 };
 
-
-// create bubble chart
-
+// create bubble chart and place it in index.html
 //https://images.plot.ly/plotly-documentation/images/plotly_js_cheat_sheet.pdf?_ga=2.115454466.525573211.1607557400-742152575.1603234155
-
 function bubbleChart(selectedId){
-    var sampleData = samples.filter(function(sample){  // parse the data
+    var sampleData = samples.filter(function(sample){  // parse the data for correct id
         return sample.id == selectedId;
     });
-    otu = sampleData[0].otu_ids;
+    otu = sampleData[0].otu_ids;                        // should be only one value in list
     samp_values = sampleData[0].sample_values;
-    text = otu.map(id => `OTU  ${id}`);
+    textBar = otu.map(id => `OTU  ${id}`);              // OTU text to display on hover
 
     var trace1 = {
         x: otu,
         y:samp_values,
         mode:'markers',
-        text: text,
+        text: textBar,
         marker: {
             color: otu,
             colorscale: 'Jet',
             type: 'heatmap',
-            //opacity: otu.map(id => 0.7),
             size: samp_values
-        }
+        },
     };
 
     var layout = {
@@ -125,40 +113,41 @@ function bubbleChart(selectedId){
       };
 
     Plotly.newPlot('bubble', [trace1], layout);
+};
+
+// create the gauge chart and place in index.html
+function guageChart(selectedId) {
+    var personalData = metadata.filter(function(person){    // parse the data
+        return person.id == selectedId;                     // checking a string number with and integer (also a number)
+    });
+    var wfreq = personalData[0].wfreq;
+    
+    var data = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: wfreq,
+            title: { text: "Belly Button Washing Frequency<br>Scrubs per Week" },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: {range: [0, 9]},
+                bar: { color: "blue" },
+            steps: [
+                {range:[0,1], color:"#000000"},
+                {range:[1,2], color:"#2F4F4F"},
+                {range:[2,3], color:"#708090"},
+                {range:[3,4], color:"#696969"},
+                {range:[4,5], color:"#808080"},
+                {range:[5,6], color:"#A9A9A9"},
+                {range:[6,7], color:"#C0C0C0"},
+                {range:[7,8], color:"#D3D3D3"},
+                {range:[8,9], color:"#DCDCDC"}
+            ],
+            }
+        }
+    ];
+    
+    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } }; 
+    
+    Plotly.newPlot('gauge', data, layout);
 }
-
-
-
-// ------
-// trace2 = {
-//     x:[],
-//     y:[],
-//     marker:{
-//         color:['red','blue'],
-//         size:[]},
-//         mode:'markers'};
-
-//         Plotly.newPlot("bubble", data, layout)
-// -------
-// . Create a bubble chart that displays each sample.
-// * Use `otu_ids` for the x values.
-// * Use `sample_values` for the y values.
-// * Use `sample_values` for the marker size.
-// * Use `otu_ids` for the marker colors.
-// * Use `otu_labels` for the text values.
-
-
-
-// Display the sample metadata, i.e., an individual's demographic information.
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-
-// ++++++
-// function filteredIndividual (individual) {
-//     return individual.
-// }
-// +++++
-// Update all of the plots any time that a new sample is selected.
-// Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown below:
-
-
-// gauge charts https://plotly.com/javascript/gauge-charts/
